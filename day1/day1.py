5398d427305581a62ff73ad83b3e1ff3878237a3
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2021-12-02 22:38:45 -0500

    day1

---
+++ day1/day1.py
@@ -0,0 +1,20 @@
+
+def increasing_measurements(sonar_readings_file):
+    sonar_readings = open(sonar_readings_file, "r")
+    
+    increasing_value_counter = 0
+    previous_value = float("inf")
+    
+    for value in sonar_readings:
+        
+        if int(value) > previous_value: increasing_value_counter += 1
+        previous_value = int(value)
+
+    sonar_readings.close()
+    
+    return increasing_value_counter
+
+if __name__ == "__main__":
+    print(increasing_measurements('day1/sonarreadings.txt'))
+
+
