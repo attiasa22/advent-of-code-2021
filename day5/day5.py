8e7bd1e0cb59edf616ed214010a3c569656b26f0
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2021-12-08 22:07:11 -0500

    day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,19 +1,62 @@
+import numpy as np
+import sys
+import re
 """Day 5 of https://adventofcode.com 2021 challenge"""
-def increasing_measurements(sonar_readings_file):
+def increasing_measurements(lines_file):
     '''Determine if sonar depth readings are increasing relative to the previous reading'''
-    with open(sonar_readings_file, "r", encoding="utf-8") as sonar_readings:
-
-        increasing_value_counter = 0
-        previous_value = float("inf")
-
-        for value in sonar_readings:
-
-            if int(value) > previous_value:
-                increasing_value_counter += 1
-
-            previous_value = int(value)
-
-    return increasing_value_counter
+    seabed = np.zeros((1000, 1000))
+    
+    with open(lines_file, "r", encoding="utf-8") as lines:
+        for value in lines:
+            value = re.split("-> |,|\n", value)
+            x1, y1 = int(value[0]), int(value[1])
+            x2, y2 = int(value[2]), int(value[3])
+            
+            
+            if x1 == x2:
+                y1, y2 = min(y1,y2), max(y1,y2)
+                #print(x1,y1,x2,y2)
+                seabed[y1 :y2+1,x2] = seabed[y1 :y2+1,x2]+1
+                #print(seabed[y1 :y2+1,x2])
+            elif y1 == y2:
+                x1, x2 = min(x1,x2), max(x1,x2)
+                #print(value)
+                seabed[y1, x1:x2+1] = seabed[y1, x1:x2+1]+1
+            
+            elif abs(x2-x1) ==abs(y2-y1):
+                if x1<x2:
+                    current_pos_y = y1
+                    current_pos_x = x1
+                    if y1<y2:
+                        
+                        while current_pos_y <=y2:
+                            seabed[current_pos_y,current_pos_x]=seabed[current_pos_y,current_pos_x]+1
+                            current_pos_y += 1
+                            current_pos_x += 1
+                    else:
+                         while current_pos_y >=y2:
+                            seabed[current_pos_y,current_pos_x]=seabed[current_pos_y,current_pos_x]+1
+                            current_pos_y -= 1
+                            current_pos_x += 1
+                else: #x1>x2
+                    current_pos_y = y1
+                    current_pos_x = x1
+                    if y1<y2:
+                        
+                        while current_pos_y <=y2:
+                            seabed[current_pos_y,current_pos_x]=seabed[current_pos_y,current_pos_x]+1
+                            current_pos_y += 1
+                            current_pos_x -= 1
+                    else:
+                        print(value)
+                        while current_pos_y >=y2:
+                            seabed[current_pos_y,current_pos_x]=seabed[current_pos_y,current_pos_x]+1
+                            current_pos_y -= 1
+                            current_pos_x -= 1
+           
+        print((np.asarray(seabed) >= 2).sum())
 
 if __name__ == "__main__":
-    print(increasing_measurements('day1/sonarreadings.txt'))
+    #np.set_printoptions(threshold=300)
+    print(increasing_measurements('day5/lines.txt'))
+
